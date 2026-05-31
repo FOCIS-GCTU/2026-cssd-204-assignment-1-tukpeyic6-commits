4517de1e592f3d29a630458780dcc8355a2eed8e
Christabel Akpene <2425401404@live.gctu.edu.gh>
2026-05-31 08:57:17 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,28 @@
-
 def main():
-  pass #code goes here
+    # File: initials.py
+    # Description : Print out my initials.
+    # Assignment Number : 1
+    #
+    # Name : Chritabel Akpene Tukpeyi
+    # STUDENT ID : 2425401404
+    # Grader : AUgustus Buckman
+    #
+    # On my honor,Christabel Akene Tukpeyi, this programming assignment is my own work
+    # and I have not provided this code to any other student.
 
-
+    print("...CAT")
+    print("")
+    print(".......CCCCCC..........AAAAAAAA..........TTTTTTTTTTTT...")
+    print(".....CCCC..CCCC.......AAAAAAAAAA..............TT........")
+    print("...CCCC..............AA........AA.............TT........")
+    print("...CCCC..............AA........AA.............TT........")
+    print("...CCCC..............AAAAAAAAAAAA.............TT........")
+    print("...CCCC..............AAAAAAAAAAAA.............TT........")
+    print("...CCCC..............AA........AA.............TT........")
+    print("...CCCC..............AA........AA.............TT........")
+    print(".....CCCC..CCCC..**..AA........AA.......**....TT......**")
+    print(".......CCCCCC....**..AA........AA.......**....TT......**")
+    print("")
+    print("")
 
 main()
